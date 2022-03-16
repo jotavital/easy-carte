7b5bff984abf3770a8c4e88f49463b1c04927a48
João Pedro Lisboa Vital <80636554+jotavital@users.noreply.github.com>
2022-03-15 21:08:19 -0300

    feat: :art: improve register form

--- easy-carte-front/src/components/Register.js
+++ easy-carte-front/src/components/Register.js
@@ -1,16 +1,26 @@
-import { Grid, Typography, Box } from "@mui/material";
+import { useTheme } from '@mui/material';
+import { Grid, Typography, Button } from "@mui/material";
 import { TextField } from "@mui/material";
 
 function Register() {
+
+    const theme = useTheme();
+
     return (
         <Grid className="full-body-height" item xs={12} container alignItems='center' justifyContent='center'>
-            <Grid item xs={6}>
-                <Typography variant="h3" textAlign='center' >Cadastro</Typography>
-                <Grid mt={3} xs={12} container justifyContent='center'>
-                    <TextField label='Nome' type='text' required />
+            <Grid item xs={10} sm={6} md={4} lg={3} >
+                <Typography variant="h3" textAlign='center'>Cadastro</Typography>
+                <Grid mt={2}>
+                    <TextField fullWidth label='Nome completo' type='text' required />
                 </Grid>
-                <Grid mt={2} xs={12} container justifyContent='center'>
-                    <TextField label='Senha' type='password' required />
+                <Grid mt={2}>
+                    <TextField fullWidth label='Nome de usuário' type='text' required />
+                </Grid>
+                <Grid mt={2}>
+                    <TextField fullWidth label='Senha' type='password' required />
+                </Grid>
+                <Grid mt={2} container justifyContent='center'>
+                    <Button variant="contained" color='success'>Enviar</Button>
                 </Grid>
             </Grid>
         </Grid>

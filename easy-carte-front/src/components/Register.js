9f4aa0707a7bf31dd58c15350bf9901bd277dc61
João Pedro Lisboa Vital <80636554+jotavital@users.noreply.github.com>
2022-04-04 22:19:23 -0300

    feat: :art: avancando no form de cadastro com useForm

--- easy-carte-front/src/components/Register.js
+++ easy-carte-front/src/components/Register.js
@@ -4,50 +4,43 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
 import { useState } from 'react';
 import ptBrLocale from 'date-fns/locale/pt-BR';
-import { format } from 'date-fns';
+import { format as formatDate } from 'date-fns';
+import { useForm, Controller } from 'react-hook-form';
 
 function Register() {
+    const { register, handleSubmit, control, formState: { errors } } = useForm();
 
-    const [formValues, setFormValues] = useState({});
-    const [birthDate, setBirthDate] = useState('');
-
-    const handleInputChanges = (e) => {
-        const { name, value } = e.target;
-        setFormValues({ ...formValues, [name]: value });
-    }
-
-    const handleBirthDateChanges = (value) => {
-        setFormValues({ ...formValues, 'birthDate': value });
-        setBirthDate(value);
-    }
-
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-    }
-
-    console.log(formValues);
+    const onSubmit = data => console.log(data);
 
     return (
         <LocalizationProvider dateAdapter={AdapterDateFns} locale={ptBrLocale} >
             <Grid className="full-body-height" item xs={12} container alignItems='center' justifyContent='center'>
                 <Grid item xs={10} sm={6} md={4} lg={3} >
-                    <form onSubmit={handleFormSubmit}>
+                    <Typography variant="h3" mb={3} textAlign='center'>Cadastro</Typography>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <Stack spacing={3}>
-                            <Typography variant="h3" textAlign='center'>Cadastro</Typography>
-                            <TextField fullWidth label='Nome completo' type='text' name="full_name" onChange={handleInputChanges} required />
-                            <DatePicker
-                                disableFuture
-                                label="Data de nascimento"
-                                openTo="year"
-                                views={['year', 'month', 'day']}
-                                onChange={(value) => {
-                                    handleBirthDateChanges(value);
-                                }}
-                                value={birthDate}
-                                renderInput={(props) => <TextField {...props} />}
+                            <TextField fullWidth label='Nome completo' type='text' {...register('full_name', { required: true })} />
+                            {errors.full_name && "Campo obrigatório"}
+                            <Controller
+                                control={control}
+                                name="birth_date"
+                                defaultValue=""
+                                render={({ field: { onChange, value } }) => (
+                                    <DatePicker
+                                        disableFuture
+                                        label="Data de nascimento"
+                                        openTo="year"
+                                        views={['year', 'month', 'day']}
+                                        onChange={onChange}
+                                        value={value}
+                                        renderInput={(props) => <TextField {...props} />}
+                                    />
+                                )}
                             />
-                            <TextField fullWidth label='E-mail' type='email' name='email' onChange={handleInputChanges} required />
-                            <TextField fullWidth label='Senha' type='password' name='password' onChange={handleInputChanges} required />
+                            <TextField fullWidth label='E-mail' type='email' {...register('email', { required: true })} />
+                            {errors.email && "Campo obrigatório"}
+                            <TextField fullWidth label='Senha' type='password' {...register('password', { required: true })} />
+                            {errors.password && "Campo obrigatório"}
                             <Grid mt={2} container justifyContent='center'>
                                 <Button variant='contained' color='success' type='submit'>Enviar</Button>
                             </Grid>

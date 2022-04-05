0aa3895e52709e188246bcc4d79133cdb1525b16
João Pedro Lisboa Vital <80636554+jotavital@users.noreply.github.com>
2022-04-05 14:58:19 -0300

    feat: :art: moving localization provider to register formk

--- easy-carte-front/src/components/Register.js
+++ easy-carte-front/src/components/Register.js
@@ -1,19 +1,14 @@
 import { Grid, Typography } from "@mui/material";
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import ptBrLocale from 'date-fns/locale/pt-BR';
 import RegisterForm from "./forms/RegisterForm";
 
 function Register() {
     return (
-        <LocalizationProvider dateAdapter={AdapterDateFns} locale={ptBrLocale} >
-            <Grid className="full-body-height" item xs={12} container alignItems='center' justifyContent='center'>
-                <Grid item xs={10} sm={6} md={4} lg={3} >
-                    <Typography variant="h3" mb={3} textAlign='center'>Cadastro</Typography>
-                    <RegisterForm />
-                </Grid>
+        <Grid className="full-body-height" item xs={12} container alignItems='center' justifyContent='center'>
+            <Grid item xs={10} sm={6} md={4} lg={3} >
+                <Typography variant="h3" mb={3} textAlign='center'>Cadastro</Typography>
+                <RegisterForm />
             </Grid>
-        </LocalizationProvider>
+        </Grid>
     );
 }
 
--- easy-carte-front/src/components/forms/RegisterForm.js
+++ easy-carte-front/src/components/forms/RegisterForm.js
@@ -2,6 +2,9 @@ import { Grid, Button, TextField, Stack } from "@mui/material";
 import { useForm, Controller } from 'react-hook-form';
 import { format as formatDate } from 'date-fns';
 import DatePicker from '@mui/lab/DatePicker';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import ptBrLocale from 'date-fns/locale/pt-BR';
 
 function RegisterForm() {
 
@@ -12,37 +15,39 @@ function RegisterForm() {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <Stack spacing={3}>
-                <TextField fullWidth label='Nome completo' type='text' {...register('full_name', { required: true })} />
-                {errors.full_name && "Campo obrigatório"}
-                <Controller
-                    control={control}
-                    name="birth_date"
-                    defaultValue={null}
-                    rules={{ required: true }}
-                    render={({ field: { onChange, value } }) => (
-                        <DatePicker
-                            disableFuture
-                            label="Data de nascimento"
-                            openTo="year"
-                            views={['year', 'month', 'day']}
-                            onChange={onChange}
-                            value={value}
-                            renderInput={(props) => <TextField {...props} />}
-                        />
-                    )}
-                />
-                {errors.birth_date && "Campo obrigatório"}
-                <TextField fullWidth label='E-mail' type='email' {...register('email', { required: true })} />
-                {errors.email && "Campo obrigatório"}
-                <TextField fullWidth label='Senha' type='password' {...register('password', { required: true })} />
-                {errors.password && "Campo obrigatório"}
-                <Grid mt={2} container justifyContent='center'>
-                    <Button variant='contained' color='success' type='submit'>Enviar</Button>
-                </Grid>
-            </Stack>
-        </form>
+        <LocalizationProvider dateAdapter={AdapterDateFns} locale={ptBrLocale} >
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Stack spacing={3}>
+                    <TextField fullWidth label='Nome completo' type='text' {...register('full_name', { required: true })} />
+                    {errors.full_name && "Campo obrigatório"}
+                    <Controller
+                        control={control}
+                        name="birth_date"
+                        defaultValue={null}
+                        rules={{ required: true }}
+                        render={({ field: { onChange, value } }) => (
+                            <DatePicker
+                                disableFuture
+                                label="Data de nascimento"
+                                openTo="year"
+                                views={['year', 'month', 'day']}
+                                onChange={onChange}
+                                value={value}
+                                renderInput={(props) => <TextField {...props} />}
+                            />
+                        )}
+                    />
+                    {errors.birth_date && "Campo obrigatório"}
+                    <TextField fullWidth label='E-mail' type='email' {...register('email', { required: true })} />
+                    {errors.email && "Campo obrigatório"}
+                    <TextField fullWidth label='Senha' type='password' {...register('password', { required: true })} />
+                    {errors.password && "Campo obrigatório"}
+                    <Grid mt={2} container justifyContent='center'>
+                        <Button variant='contained' color='success' type='submit'>Enviar</Button>
+                    </Grid>
+                </Stack>
+            </form>
+        </LocalizationProvider>
     );
 }
 

176aba193da86f93b5a4ce546b61a8f4e61088d3
João Pedro Vital <joaopedrovitaldoctum@gmail.com>
2022-04-16 00:48:03 -0300

    fix: :art: config do restore-terminals

--- easy-carte-api/routes/api.php
+++ easy-carte-api/routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test/user', [UserController::class, 'index']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });

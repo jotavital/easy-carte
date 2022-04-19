ae45252365ffc0dafb1d2d19e0c050fc06b13535
João Pedro Vital <joaopedrovitaldoctum@gmail.com>
2022-04-19 18:13:36 -0300

    feat: :art: will try sanctum

--- easy-carte-api/routes/api.php
+++ easy-carte-api/routes/api.php
@@ -17,10 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/test/user', [UserController::class, 'index']);
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::resource('user', UserController::class);
+Route::middleware('auth:sanctum')->resource('/user', UserController::class);
 
 Route::post('/login', [UserController::class, 'login']);

8dfdfb6c97830552e9a3a85d0ab5126e668f7171
João Pedro Vital <joaopedrovitaldoctum@gmail.com>
2022-06-27 18:08:44 -0300

    fix: :bug: adjusts in search input
    r

--- easy-carte-front/src/components/forms/inputs/SearchInput.jsx
+++ easy-carte-front/src/components/forms/inputs/SearchInput.jsx
@@ -1,32 +1,20 @@
-import { Autocomplete, TextField } from "@mui/material";
+import { InputAdornment, TextField } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 function SearchInput() {
     return (
-        <Autocomplete
+        <TextField
+            label="Onde você vai comer hoje?"
             fullWidth
-            freeSolo
-            id="search-input"
-            options={top100Films.map((option) => option.label)}
-            renderInput={
-                (params) =>
-                    <TextField
-                        {...params}
-                        label="Onde você quer comer hoje?"
-                        size="small"
-                    />
-            }
+            InputProps={{
+                endAdornment: (
+                    <InputAdornment position="start">
+                        <SearchIcon />
+                    </InputAdornment>
+                ),
+            }}
         />
     );
 }
-
-const top100Films = [
-    { label: 'The Shawshank Redemption', year: 1994 },
-    { label: 'The Godfather', year: 1972 },
-    { label: 'The Godfather: Part II', year: 1974 },
-    { label: 'The Dark Knight', year: 2008 },
-    { label: '12 Angry Men', year: 1957 },
-    { label: "Schindler's List", year: 1993 },
-    { label: 'Pulp Fiction', year: 1994 }
-];
 
 export default SearchInput;

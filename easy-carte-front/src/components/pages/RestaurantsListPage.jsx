f4a3973615d6bacf1cbcda25fb189c214d17c7dd
João Pedro Vital <joaopedrovitaldoctum@gmail.com>
2022-09-08 18:58:40 -0300

    feat: :art: ajustes na pagina de codigo do restaurante

--- easy-carte-front/src/components/pages/RestaurantsListPage.jsx
+++ easy-carte-front/src/components/pages/RestaurantsListPage.jsx
@@ -1,18 +1,15 @@
-import { useNavigate, useParams } from "react-router-dom";
-import { Grid, Stack, TextField, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 import HomeRestaurantsSection from '../sections/HomeRestaurantsSection';
 import SearchInput from '../forms/inputs/SearchInput';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectCity } from "../../redux/appSlice";
-import { setSnackbar } from "../../redux/snackbars/snackbarsSlice";
 import WhereAreYou from '../sections/WhereAreYou';
 import { useEffect, useState } from "react";
 import CategoriesListWithIcon from "../lists/CategoriesListWithIcon";
 import { apiClient } from "../../providers/apiClient";
 import UserLocationText from '../text/UserLocationText';
-import Image from "../images/Image";
-import CustomButton from "../buttons/CustomButton";
-import { useForm } from "react-hook-form";
+import EnterRestaurantCode from "../sections/EnterRestaurantCode";
+import { useParams } from 'react-router-dom';
 
 function RestaurantsListPage() {
     const dispatch = useDispatch();
@@ -21,24 +18,10 @@ function RestaurantsListPage() {
     const [userLocation, setUserLocation] = useState(localStorage.getItem('user_location') ?? null);
     const [areCategoriesLoaded, setAreCategoriesLoaded] = useState(false);
     const [categories, setCategories] = useState({});
-    const { handleSubmit, formState: { errors }, register, setError } = useForm();
-    const navigate = useNavigate();
 
     const handleUserLocationChanged = (location) => {
         setUserLocation(location);
         localStorage.setItem('user_location', location);
-    }
-
-    const onSubmit = ({ code }) => {
-        apiClient.get('/restaurants/check-code/' + code)
-            .then(({ data }) => {
-                if (data) {
-                    navigate('/restaurants/' + data.id + '/products');
-                } else {
-                    dispatch(setSnackbar(true, 'error', 'Código de restaurante inválido'));
-                    setError('code');
-                }
-            });
     }
 
     useEffect(() => {
@@ -71,50 +54,7 @@ function RestaurantsListPage() {
                 </>
                 :
                 (userLocation === 'restaurant') ?
-                    <Grid marginBottom={3} item container justifyContent='center' alignItems='center'>
-                        <Grid container item sm={8} justifyContent='center' padding={2}>
-                            <Grid item padding sm={5}>
-                                <Image src="./illustrations/woman_cellphone.svg" />
-                            </Grid>
-                            <Grid container padding justifyContent='center'>
-                                <Typography variant="h5" fontWeight='bold'>
-                                    Código do restaurante
-                                </Typography>
-                            </Grid>
-                            <Grid container justifyContent='center'>
-                                <Typography>
-                                    Você deve encontrar o código do restaurante nas mesas ou no balcão
-                                </Typography>
-                            </Grid>
-                            <form noValidate onSubmit={handleSubmit(onSubmit)}>
-                                <Stack spacing={3}>
-                                    <TextField
-                                        fullWidth
-                                        error={!!errors.code}
-                                        helperText={errors.code?.message}
-                                        label='Código do restaurante'
-                                        {...register('code',
-                                            {
-                                                required: {
-                                                    value: true,
-                                                    message: "Informe o código do restaurante"
-                                                },
-                                                maxLength: {
-                                                    value: 4,
-                                                    message: 'Até 4 caracteres'
-                                                },
-                                                pattern: {
-                                                    value: /^[0-9]+$/,
-                                                    message: 'Apenas números',
-                                                }
-                                            }
-                                        )}
-                                    />
-                                    <CustomButton text='Pronto' color='primary' type='submit' />
-                                </Stack>
-                            </form>
-                        </Grid>
-                    </Grid>
+                    <EnterRestaurantCode />
                     :
                     <WhereAreYou handleUserLocationChanged={handleUserLocationChanged} />
             }
---
+++ easy-carte-front/src/components/sections/EnterRestaurantCode.jsx
@@ -0,0 +1,75 @@
+import { Grid, Stack, TextField, Typography } from "@mui/material";
+import CustomButton from "../buttons/CustomButton";
+import Image from "../images/Image";
+import { useForm } from "react-hook-form";
+import { apiClient } from "../../providers/apiClient";
+import { useNavigate } from "react-router-dom";
+import { setSnackbar } from '../../redux/snackbars/snackbarsSlice';
+import { useDispatch } from "react-redux";
+
+function EnterRestaurantCode() {
+    const { handleSubmit, formState: { errors }, register, setError } = useForm();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const onSubmit = ({ code }) => {
+        apiClient.get('/restaurants/check-code/' + code)
+            .then(({ data }) => {
+                if (data) {
+                    navigate('/restaurants/' + data.id + '/products');
+                } else {
+                    dispatch(setSnackbar(true, 'error', 'Código de restaurante inválido', 'right'));
+                    setError('code');
+                }
+            });
+    }
+
+    return (
+        <Grid marginBottom={3} item container justifyContent='center' alignItems='center'>
+            <Grid container item sm={8} justifyContent='center' padding={2}>
+                <Grid item padding sm={5}>
+                    <Image src="./illustrations/woman_cellphone.svg" />
+                </Grid>
+                <Grid container padding justifyContent='center'>
+                    <Typography variant="h5" fontWeight='bold'>
+                        Código do restaurante
+                    </Typography>
+                </Grid>
+                <Grid container justifyContent='center'>
+                    <Typography marginBottom>
+                        Você deve encontrar o código do restaurante nas mesas ou no balcão
+                    </Typography>
+                </Grid>
+                <form noValidate onSubmit={handleSubmit(onSubmit)}>
+                    <Stack spacing={3}>
+                        <TextField
+                            fullWidth
+                            error={!!errors.code}
+                            helperText={errors.code?.message}
+                            label='Código do restaurante'
+                            {...register('code',
+                                {
+                                    required: {
+                                        value: true,
+                                        message: "Informe o código do restaurante"
+                                    },
+                                    maxLength: {
+                                        value: 4,
+                                        message: 'Até 4 caracteres'
+                                    },
+                                    pattern: {
+                                        value: /^[0-9]+$/,
+                                        message: 'Apenas números',
+                                    }
+                                }
+                            )}
+                        />
+                        <CustomButton text='Pronto' color='primary' type='submit' />
+                    </Stack>
+                </form>
+            </Grid>
+        </Grid>
+    );
+}
+
+export default EnterRestaurantCode;
